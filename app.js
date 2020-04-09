c16cb8c1aa09bfff7d03766082da4e36ee460708
maicolc2009_00 <maicol.campos@iv2.com.br>
2020-04-09 17:49:48 -0300

    teste 3

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const app = express()
 app.use( (req, res) => {
-    res.status(200).send({
-        mensagem: 'Maicol Teste'
-    })
+    res.status(200).send('<h1>Isso é apenas um teste</h1>')
 })
 
 
